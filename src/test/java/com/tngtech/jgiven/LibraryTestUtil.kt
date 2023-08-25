fff9ef714f6dbffbd323382a30c7e589a416c0da
l-1squared <30831153+l-1squared@users.noreply.github.com>
2023-08-25 08:24:32 +0200

    Throw exception directly
    According to my (limited) understanding, without adding JGiven as a module the tests depending on it will not function. Therefore, continuing without a library is nonsensical. Furthermore if JGiven is supposed to be present it must be present as a jar (because tha's what we publish), therefore there is no use in trying maybe add Jgiven as a collection of class files (this should only be the case if we were in the same project)
    
    Signed-off-by: l-1sqared <30831153+l-1squared@users.noreply.github.com>

--- src/test/java/com/tngtech/jgiven/LibraryTestUtil.kt
+++ src/test/java/com/tngtech/jgiven/LibraryTestUtil.kt
@@ -10,7 +10,6 @@ import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess
 import com.intellij.testFramework.PsiTestUtil
 import com.tngtech.jgiven.annotation.ScenarioState
 import java.io.File
-import java.util.*
 
 class LibraryTestUtil constructor(private val myModule: Module) {
 
@@ -18,24 +17,29 @@ class LibraryTestUtil constructor(private val myModule: Module) {
 
     private fun addJarContaining(clazz: Class<*>): LibraryTestUtil {
         val path = getJarPath(clazz)
-        if (path != null)
-            addLibraryAt(path)
+        addLibraryAt(path)
         return this
     }
 
-    private fun getJarPath(clazz: Class<*>): String? {
+    private fun getJarPath(clazz: Class<*>): String {
         val className = clazz.name.replace('.', '/') + ".class"
-        val classPath = clazz.classLoader.getResource(className)?.toString() ?: return null
+        val classPath = clazz.classLoader.getResource(className)?.toString()
+                ?: throw IllegalArgumentException("Class $clazz not found")
         return if (classPath.startsWith("jar")) {
             classPath.substringAfter("jar:file:").substringBefore('!')
         } else {
-            null
+            throw IllegalArgumentException("Class $clazz is not in a jar file")
+
         }
     }
 
 
     fun addJGiven(): LibraryTestUtil {
-        return addJarContaining(ScenarioState::class.java)
+        try {
+            return addJarContaining(ScenarioState::class.java)
+        }catch(e: IllegalArgumentException){
+            throw IllegalArgumentException("JGiven jar not found. Please add it to the classpath")
+        }
     }
 
     private fun addLibraryAt(path: String) {
@@ -43,9 +47,11 @@ class LibraryTestUtil constructor(private val myModule: Module) {
         val file = File(path)
         VfsRootAccess.allowRootAccess(myModule, parts[0])
         val fileName = file.name
-        ModuleRootModificationUtil.updateModel(myModule) { model -> references.add(Ref.create(PsiTestUtil.addLibrary(
-            model, fileName, file.parent, fileName
-        ))) }
+        ModuleRootModificationUtil.updateModel(myModule) { model ->
+            references.add(Ref.create(PsiTestUtil.addLibrary(
+                    model, fileName, file.parent, fileName
+            )))
+        }
     }
 
     internal fun removeLibraries() {
