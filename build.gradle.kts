1ca5d478e0867b916d0c614f4abc631ca907a854
Christian Oertel <christian.oertel@partner.bmwgroup.com>
2024-10-04 16:26:20 +0200

    JGiven-10: migrate to new gradle provider
    Signed-off-by: Christian Oertel <christian.oertel@partner.bmwgroup.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 import org.jetbrains.kotlin.gradle.plugin.getKotlinPluginVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -5,7 +6,7 @@ val jetbrainsPublishUsername: String? by project
 val jetbrainsPublishToken: String? by project
 
 plugins {
-    id("org.jetbrains.intellij") version "1.15.0"
+    id("org.jetbrains.intellij.platform") version "2.1.0"
     id("org.jetbrains.kotlin.jvm") version "2.1.0-Beta1"
 }
 val kotlinVersion = project.getKotlinPluginVersion()
@@ -26,15 +27,27 @@ tasks {
 
 repositories {
     mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
-intellij {
-    version.set("IC-2023.3")
-    pluginName.set("jgiven-intellij-plugin")
-    plugins.set(listOf("java"))
+intellijPlatform {
+    pluginConfiguration {
+        name = "jgiven-intellij-plugin"
+    }
 }
 
 dependencies {
+    intellijPlatform {
+        intellijIdeaCommunity("2024.2")
+        bundledPlugin("com.intellij.java")
+        instrumentationTools()
+
+        testFramework(TestFrameworkType.Plugin.Java)
+    }
+
     implementation(kotlin("stdlib", kotlinVersion))
 
     testImplementation(kotlin("stdlib-jdk7", kotlinVersion))
@@ -42,6 +55,8 @@ dependencies {
     testImplementation("com.tngtech.junit.dataprovider:junit4-dataprovider:2.10")
     testImplementation("org.assertj:assertj-core:3.26.3")
     testImplementation("org.mockito:mockito-core:5.14.1")
+
+    testImplementation("org.opentest4j:opentest4j:1.3.0")
 }
 
 inline operator fun <T : Task> T.invoke(a: T.() -> Unit): T = apply(a)
