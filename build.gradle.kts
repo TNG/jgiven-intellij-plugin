09d53f55cd769099e95abec136c743f5b2e653c1
Christian Oertel <christian.oertel@partner.bmwgroup.com>
2026-08-07 13:27:53 +0200

    Update plugin to support newest IntelliJ version
    Signed-off-by: Christian Oertel <christian.oertel@partner.bmwgroup.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,22 +6,28 @@ val jetbrainsPublishUsername: String? by project
 val jetbrainsPublishToken: String? by project
 
 plugins {
-    id("org.jetbrains.intellij.platform") version "2.1.0"
-    id("org.jetbrains.kotlin.jvm") version "2.0.21"
+    id("org.jetbrains.intellij.platform") version "2.18.1"
+    id("org.jetbrains.kotlin.jvm") version "2.4.10"
 }
 val kotlinVersion = project.getKotlinPluginVersion()
 
 configure<JavaPluginExtension> {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks {
     withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+        }
     }
     buildSearchableOptions {
         enabled = false
+    }
+    patchPluginXml {
+        sinceBuild.set("262")
+        untilBuild.set(provider { null })
     }
 }
 
@@ -30,14 +36,17 @@ repositories {
 
     intellijPlatform {
         defaultRepositories()
+        snapshots()
     }
 }
 
 dependencies {
     intellijPlatform {
-        intellijIdeaCommunity("2024.2")
+        intellijIdea("262-EAP-SNAPSHOT") {
+            useInstaller = false
+        }
+        jetbrainsRuntime()
         bundledPlugin("com.intellij.java")
-        instrumentationTools()
 
         testFramework(TestFrameworkType.Plugin.Java)
     }
