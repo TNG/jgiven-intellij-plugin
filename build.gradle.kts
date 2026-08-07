9cacd47e4b2c30332830b803f1584e62919ba1d6
Christian Oertel <christian.oertel@partner.bmwgroup.com>
2026-08-07 13:51:52 +0200

    Update test versions
    Signed-off-by: Christian Oertel <christian.oertel@partner.bmwgroup.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
         jetbrainsRuntime()
         bundledPlugin("com.intellij.java")
 
+        testFramework(TestFrameworkType.Platform)
         testFramework(TestFrameworkType.Plugin.Java)
     }
 
@@ -57,7 +58,7 @@ dependencies {
     testImplementation("com.tngtech.jgiven:jgiven-junit:1.3.0")
     testImplementation("com.tngtech.junit.dataprovider:junit4-dataprovider:2.10")
     testImplementation("org.assertj:assertj-core:3.26.3")
-    testImplementation("org.mockito:mockito-core:5.14.1")
+    testImplementation("org.mockito:mockito-core:5.18.0")
 
     testImplementation("org.opentest4j:opentest4j:1.3.0")
 }
